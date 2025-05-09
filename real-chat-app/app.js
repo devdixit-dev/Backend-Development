78dc95680acb57effd83d3a78866763822daf850
Dev Dixit <devdixitsocial@gmail.com>
2025-05-09 19:27:45 +0530

    chat app complete

--- real-chat-app/app.js
+++ real-chat-app/app.js
@@ -1,12 +1,23 @@
+import path from 'path'
 import express from 'express';
 import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const port = 9000
 const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.resolve('./public')));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, world</h1>');
+  res.sendFile('/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(port, () => {
