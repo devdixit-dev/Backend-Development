12bf0f27e84c5c608ca26b1f8e269dd605f7159a
Dev Dixit <devdixitsocial@gmail.com>
2025-05-13 09:04:22 +0530

    server setup completed

--- 4.graphql/server/app.js
+++ 4.graphql/server/app.js
@@ -1,53 +1,24 @@
-import express from 'express';
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import cors from 'cors';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-const typeDefs = `
-  type Todo {
-    id: ID!
-    title: String!
-    completed: Boolean
-  }
+const port = 4444;
 
-  type Query {
-    getTodos: [Todo]
-  }
-`;
+const server = new ApolloServer({
+  typeDefs: `type Query { hello: String, hello2: String }`,
+  resolvers:{
+    Query: {
+      hello: () => "Hello world",
+      hello2: () => "Hello world 2",
+    },
+  },
+});
 
-const resolvers = {
-  Query: {
-    getTodos: () => [
-      { id: '1', title: 'Learn GraphQL', completed: false },
-      { id: '2', title: 'Build something cool', completed: true }
-    ]
-  }
-};
-
-async function startServer() {
-  const app = express();
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-
-  await server.start();
-
-  // Middlewares BEFORE /graphql
-  app.use(cors());
-  app.use(express.json());
-
-  app.get(
-    '/graphql',
-    expressMiddleware(server, {
-      context: async ({ req }) => ({ token: req.headers.authorization })
-    })
-  );
-
-  app.listen(8000, () => {
-    console.log('Server ready at http://localhost:8000/graphql');
-  });
-}
-
-startServer();
+startStandaloneServer(server, {
+  listen: { port }
+})
+.then(() => {
+  console.log(`Server is working on port ${port}`);
+})
+.catch((err) => {
+  console.error(err);
+});
