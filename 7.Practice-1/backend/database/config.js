19800319f6493d285c6598d5d78b27c68c10d874
Dev Dixit <devdixitsocial@gmail.com>
2025-05-20 19:30:05 +0530

    update folder

--- 7.Practice-1/backend/database/config.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI, {dbName: process.env.DB_NAME})
-  .then(() => { console.log(`Database connected`) })
-  .catch((e) => { console.log(`Error while connecting database: ${e}`) });
-}
-
-export default connectDB;
