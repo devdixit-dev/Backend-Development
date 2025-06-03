a3616522313ec9b486c095b0b44c167b5fdb016d
Dev Dixit <devdixitsocial@gmail.com>
2025-06-03 07:56:38 +0530

    server ready

---
+++ Youtube/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world');
+});
+
+app.listen(port, () => {
+  console.log(`Express running on port ${port}`);
+});
