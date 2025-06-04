165a960a8793e6f4a3036b9988103b323f98169d
Dev Dixit <devdixitsocial@gmail.com>
2025-06-04 08:53:56 +0530

    setup frontend for API

--- Youtube/backend/app.js
+++ Youtube/backend/app.js
@@ -8,6 +8,36 @@ app.get('/', (req, res) => {
   res.send('Hello, world');
 });
 
+app.get('/api/jokes', (req, res) => {
+  res.json(
+    {
+      id: 1,
+      title: 'Joke 1',
+      content: 'Joke 1 content'
+    },
+    {
+      id: 2,
+      title: 'Joke 2',
+      content: 'Joke 2 content'
+    },
+    {
+      id: 3,
+      title: 'Joke 3',
+      content: 'Joke 3 content'
+    },
+    {
+      id: 4,
+      title: 'Joke 4',
+      content: 'Joke 4 content'
+    },
+    {
+      id: 5,
+      title: 'Joke 5',
+      content: 'Joke 5 content'
+    },
+  )
+});
+
 app.listen(port, () => {
   console.log(`Express running on port ${port}`);
 });
--- Youtube/frontend/src/App.jsx
+++ Youtube/frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
+
 const App = () => {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {}, [])
+
   return (
-    <div>
-      <h2>Jokes API</h2>
+    <div className="h-screen flex justify-center items-center flex-col">
+      <h2 className="text-2xl">Jokes API</h2>
+      <p>Jokes length: {jokes.length}</p>
+
+      {jokes.map((joke) => {
+        <div key={joke.id}>
+          <p>{joke.title}</p>
+          <p>{joke.content}</p>
+        </div>
+      })}
     </div>
   )
 }
