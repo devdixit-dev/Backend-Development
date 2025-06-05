b5aea616949d2c6799988ad8e7ccbf4f459fc46a
Dev Dixit <devdixitsocial@gmail.com>
2025-06-05 19:36:14 +0530

    update frontend with typescript

---
+++ Youtube/frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+const App = () => {
+
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/jokes')
+    .then((response) => { setJokes(response.data) })
+    .catch((e) => { console.log(e) })
+  })
+
+  return (
+    <div className="h-screen flex justify-center flex-col items-center">
+      <h3>Jokes list</h3>
+      <p>{jokes.length}</p>
+      {
+        jokes.map((joke) => (
+          <div key={joke.id}>
+            <h4>{joke.title}</h4>
+            <h4>{joke.content}</h4>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default App
