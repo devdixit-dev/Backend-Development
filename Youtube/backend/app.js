6a25d93dab96c34a2da08286bd7f22dc05470e2f
Dev Dixit <devdixitsocial@gmail.com>
2025-06-07 08:39:57 +0530

    use cors and frontend and backend integration

--- Youtube/backend/app.js
+++ Youtube/backend/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello, world');
--- Youtube/frontend/src/App.tsx
+++ Youtube/frontend/src/App.tsx
@@ -9,15 +9,15 @@ const App = () => {
     axios.get('http://localhost:3000/api/jokes')
     .then((response) => { setJokes(response.data) })
     .catch((e) => { console.log(e) })
-  })
+  }, [])
 
   return (
     <div className="h-screen flex justify-center flex-col items-center">
       <h3>Jokes list</h3>
       <p>{jokes.length}</p>
       {
-        jokes.map((joke) => (
-          <div key={joke.id}>
+        jokes.map((joke, index) => (
+          <div key={index}>
             <h4>{joke.title}</h4>
             <h4>{joke.content}</h4>
           </div>
