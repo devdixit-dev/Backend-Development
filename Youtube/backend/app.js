3dfcbff33bd2858d2c02b3b58c88c2c8a9b3a957
Dev Dixit <devdixitsocial@gmail.com>
2025-06-11 08:47:12 +0530

    backend setup ready

--- Youtube/backend/app.js
+++ Youtube/backend/app.js
@@ -5,44 +5,8 @@ import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
-
 app.get('/', (req, res) => {
   res.send('Hello, world');
-});
-
-app.get('/api/jokes', (req, res) => {
-  const jokes = [
-    {
-      id: 1,
-      title: 'Joke 1',
-      content: 'Joke 1 content'
-    },
-    {
-      id: 2,
-      title: 'Joke 2',
-      content: 'Joke 2 content'
-    },
-    {
-      id: 3,
-      title: 'Joke 3',
-      content: 'Joke 3 content'
-    },
-    {
-      id: 4,
-      title: 'Joke 4',
-      content: 'Joke 4 content'
-    },
-    {
-      id: 5,
-      title: 'Joke 5',
-      content: 'Joke 5 content'
-    },
-  ]
-  res.send(jokes)
 });
 
 app.listen(port, () => {
