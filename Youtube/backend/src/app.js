8f19c39da5a3ab6febd077397df67ba5f071920a
Dev Dixit <devdixitsocial@gmail.com>
2025-06-16 09:00:52 +0530

    app.use and async handler in utils

--- Youtube/backend/src/app.js
+++ Youtube/backend/src/app.js
@@ -10,7 +10,8 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: "20kb" }));
-
+app.use(express.urlencoded({extended: true, limit: "20kb"}));
+app.use(express.static("public"))
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
